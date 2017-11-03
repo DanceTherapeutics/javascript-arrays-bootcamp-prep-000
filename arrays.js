883580c6f5945ef15ec1c3add08232758bc27ee8
Chris Junker <dancetherapeutics@gmail.com>
2017-11-03 12:21:58 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,22 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+function addElementToBeginningOfArray(array, element){
+  return [element,...array];
+}
+function destructivelyAddElementToBeginningOfArray(array, element){
+  return array.unshift(element);
+}
+function addElementToEndOfArray(array, element){
+  return [...array, element];
+}
+function destructivelyAddElementToEndOfArray(array, element){
+  return array.push(element);
+}
+function accessElementInArray(array, element){
+  return array[2];
+}
+function removeElementFromBeginningOfArray(array, element){
+  return array.slice(1);
+}
+function removeElementFromEndOfArray(array, element){
+  return array.slice(0, array.length - 1);
+}
